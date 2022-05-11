b5b216f4f0c53f9373f2284ed07a08a0e0b503be
M4ss1ck <4ndyraul@gmail.com>
2022-05-11 14:53:40 -0400

    add; join or start games

--- src/components/GameView.tsx
+++ src/components/GameView.tsx
@@ -20,18 +20,66 @@ interface Player {
 
 const GameView: React.FC<{ game: Corpse }> = ({ game }) => {
   const ctx = React.useContext(AppCtx);
+
+  const startGame = (game: Corpse) => {
+    const newgame = { ...game, gameStatus: "playing" as "playing" };
+    console.log(newgame);
+    const info = `${newgame.admin.name} started "${newgame.sessionName}" in Exquisite Corpse`;
+    window.webxdc.sendUpdate({ payload: newgame }, info);
+    return newgame;
+  };
+
+  const joinGame = (game: Corpse) => {
+    const { players } = game;
+    const playerList = players.map((player) => player.address);
+    if (!playerList.includes(ctx.playerAddr)) {
+      const newPlayers = [
+        ...players,
+        { name: ctx.playerName, address: ctx.playerAddr },
+      ];
+      const newGame = { ...game, players: newPlayers };
+      // send update
+      const info = `${newGame.admin.name} joined ${newGame.sessionName}`;
+      window.webxdc.sendUpdate({ payload: newGame }, info);
+      return newGame;
+    }
+  };
   return (
-    <div>
-      <h2>{game.sessionName}</h2>
+    <>
+      <h2 className="m-2 mx-auto text-2xl font-bold">{game.sessionName}</h2>
+      <h3 className="font-bold">Players</h3>
+      <ul>
+        {game.players.map((player: Player) => {
+          return <li key={player.address}>{player.name}</li>;
+        })}
+      </ul>
       {ctx?.playerName === game.admin.name ? (
-        <p>You are admin here</p>
+        <>
+          <p>You are admin here</p>
+          <button
+            className="px-4 my-2 border border-primario rounded-xl"
+            onClick={() => ctx?.toggleCurrentGame(startGame(game))}
+          >
+            Start Game
+          </button>
+        </>
       ) : (
-        <p>Not your game</p>
+        <>
+          <p>Not your game</p>
+          <p>
+            Admin: {game.admin.name} ({game.admin.address})
+          </p>
+          {game.gameStatus === "new" ? (
+            <button
+              className="px-4 my-2 border border-primario rounded-xl"
+              onClick={() => ctx?.toggleCurrentGame(joinGame(game))}
+            >
+              Join Game
+            </button>
+          ) : null}
+        </>
       )}
-      <p>
-        Admin: {game.admin.name} ({game.admin.address})
-      </p>
-    </div>
+    </>
   );
 };
 
