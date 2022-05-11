ae03df5b949e08dd193e1c988bee174915e90ee1
M4ss1ck <4ndyraul@gmail.com>
2022-05-11 18:22:08 -0400

    add: children support for playerlist

--- src/components/PlayerList.tsx
+++ src/components/PlayerList.tsx
@@ -5,13 +5,19 @@ interface Player {
   address: string;
 }
 
-const PlayerList: React.FC<{ players: Player[] }> = ({ players }) => {
+const PlayerList: React.FC<{ players: Player[]; children?: JSX.Element }> = ({
+  players,
+  children,
+}) => {
   return (
-    <ul>
-      {players.map((player: Player) => {
-        return <li key={player.name}>{player.name}</li>;
-      })}
-    </ul>
+    <>
+      <ul className="flex flex-col items-center justify-center p-2 m-2 border border-black rounded-lg">
+        {children}
+        {players.map((player: Player) => {
+          return <li key={player.name}>{player.name}</li>;
+        })}
+      </ul>
+    </>
   );
 };
 
