8ff2c9f1051486864ca0668db0a6863257f73e6c
M4ss1ck <4ndyraul@gmail.com>
2022-05-12 08:43:55 -0400

    fix: remove boilerplate

--- src/components/GameView.tsx
+++ src/components/GameView.tsx
@@ -27,29 +27,6 @@ const GameView: React.FC<{ game: Corpse }> = ({ game }) => {
 
   let isYourTurn = ctx.currentGame?.currentPlayer.address === ctx.playerAddr;
 
-  const startGame = (game: Corpse) => {
-    const newgame = { ...game, gameStatus: "playing" as "playing" };
-    //console.log(newgame);
-    const info = `${newgame.admin.name} started "${newgame.sessionName}" in Exquisite Corpse`;
-    window.webxdc.sendUpdate({ payload: newgame }, info);
-    return newgame;
-  };
-
-  const joinGame = (game: Corpse) => {
-    const { players } = game;
-    const playerList = players.map((player) => player.address);
-    if (!playerList.includes(ctx.playerAddr)) {
-      const newPlayers = [
-        ...players,
-        { name: ctx.playerName, address: ctx.playerAddr },
-      ];
-      const newGame = { ...game, players: newPlayers };
-      // send update
-      const info = `${newGame.admin.name} joined ${newGame.sessionName}`;
-      window.webxdc.sendUpdate({ payload: newGame }, info);
-      return newGame;
-    }
-  };
   return (
     <>
       <h2 className="m-2 mx-auto text-2xl font-bold">{game.sessionName}</h2>
@@ -58,9 +35,9 @@ const GameView: React.FC<{ game: Corpse }> = ({ game }) => {
       </PlayerList>
 
       {game.gameStatus === "new" && <GameViewNew game={game} />}
-      {game.gameStatus === "playing" && (
+      {/* {game.gameStatus === "playing" && (
         <p>It's {ctx.currentGame?.currentPlayer.name}'s turn</p>
-      )}
+      )} */}
       {game.gameStatus === "playing" && (
         <>
           {isYourTurn ? (
