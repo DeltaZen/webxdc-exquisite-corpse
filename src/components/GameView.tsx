e26726564bf2947b111cff3e85875d2924ae482b
M4ss1ck <4ndyraul@gmail.com>
2022-05-15 15:39:41 -0400

    fix: game view issues

--- src/components/GameView.tsx
+++ src/components/GameView.tsx
@@ -20,20 +20,33 @@ const GameView: React.FC<{ game: Corpse }> = ({ game }) => {
             <h3 className="font-bold">Players</h3>
           </PlayerList> */}
 
-          <PlayerList players={status.currentViewedGame?.players as Player[]}>
-            <h3 className="font-bold">Players (actual)</h3>
-          </PlayerList>
-
-          <PlayerList players={status.currentPlayingGame?.players as Player[]}>
-            <h3 className="font-bold">Players (jugando)</h3>
-          </PlayerList>
-
-          {game.gameStatus === "new" && <GameViewNew game={game} />}
+          {game.gameStatus === "new" && (
+            <>
+              <PlayerList
+                players={status.currentViewedGame?.players as Player[]}
+              >
+                <h3 className="font-bold">Players</h3>
+              </PlayerList>
+              <GameViewNew game={game} />
+            </>
+          )}
           {game.gameStatus === "playing" && (
             <>
               {status.currentViewedGame?.currentPlayer.address ===
               status.playerAddr ? (
-                <GameViewText />
+                <>
+                  <PlayerList
+                    players={
+                      status.currentViewedGame.sessionName ===
+                      status.currentPlayingGame?.sessionName
+                        ? (status.currentPlayingGame?.players as Player[])
+                        : (status.currentViewedGame.players as Player[])
+                    }
+                  >
+                    <h3 className="font-bold">Players</h3>
+                  </PlayerList>
+                  <GameViewText />
+                </>
               ) : (
                 <p>
                   It's {status.currentViewedGame?.currentPlayer.name}'s turn
