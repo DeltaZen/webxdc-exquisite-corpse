0504d557bdb9064a061ab57a7fa70c7cb727450c
M4ss1ck <4ndyraul@gmail.com>
2022-05-15 14:49:25 -0400

    fix: join text on game end

--- src/components/GameViewClosed.tsx
+++ src/components/GameViewClosed.tsx
@@ -2,12 +2,12 @@ import React from "react";
 import AppCtx from "../context/AppContext";
 
 const GameViewClosed: React.FC<{ game: Corpse }> = ({ game }) => {
+  const text = game.corpse.join(" ");
   return (
     <>
       <h2 className="m-2 mx-auto text-2xl font-bold">{game.sessionName}</h2>
-      {game.corpse.map((line, i) => (
-        <p key={i}>{line}</p>
-      ))}
+
+      <p>{text}</p>
     </>
   );
 };
