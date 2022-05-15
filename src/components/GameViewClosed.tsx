aca6158be6d3ba6a3f195b2ff6894684db0c16ff
M4ss1ck <4ndyraul@gmail.com>
2022-05-15 15:39:10 -0400

    fix: no title in game closed component

--- src/components/GameViewClosed.tsx
+++ src/components/GameViewClosed.tsx
@@ -4,8 +4,6 @@ const GameViewClosed: React.FC<{ game: Corpse }> = ({ game }) => {
   const text = game.corpse.join(" ");
   return (
     <>
-      <h2 className="m-2 mx-auto text-2xl font-bold">{game.sessionName}</h2>
-
       <p className="max-w-prose">{text}</p>
     </>
   );
