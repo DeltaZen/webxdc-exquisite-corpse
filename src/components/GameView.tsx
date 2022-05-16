beb1475e3e0df4009d9617ba7a5b071cd6f57f30
M4ss1ck <4ndyraul@gmail.com>
2022-05-16 12:39:17 -0400

    fix: useContext instead of Consumer in GameView

--- src/components/GameView.tsx
+++ src/components/GameView.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import PlayerList from "./PlayerList";
 import AppCtx from "../context/AppContext";
 import GameViewNew from "./GameViewNew";
@@ -6,59 +6,53 @@ import GameViewText from "./GameViewText";
 import GameViewClosed from "./GameViewClosed";
 
 const GameView: React.FC<{ game: Corpse }> = ({ game }) => {
+  const { status } = React.useContext(AppCtx);
+
   return (
-    <AppCtx.Consumer>
-      {({ status, setStatus }) => (
-        <>
-          <h2 className="m-2 mx-auto text-2xl font-bold">{game.sessionName}</h2>
-          {game.gameStatus === "playing" && (
-            <p>
-              ⌛ {game.currentRound}/{game.rounds} TurnID: {game.turnID}/
-              {game.players.length * game.rounds}
-            </p>
-          )}
-          {/* <PlayerList players={game.players}>
+    <>
+      <h2 className="m-2 mx-auto text-2xl font-bold">{game.sessionName}</h2>
+      {game.gameStatus === "playing" && (
+        <p>
+          ⌛ {game.currentRound}/{game.rounds} TurnID: {game.turnID}/
+          {game.players.length * game.rounds}
+        </p>
+      )}
+      {/* <PlayerList players={game.players}>
             <h3 className="font-bold">Players</h3>
           </PlayerList> */}
 
-          {game.gameStatus === "new" && (
+      {game.gameStatus === "new" && (
+        <>
+          <PlayerList players={status.currentViewedGame?.players as Player[]}>
+            <h3 className="font-bold">Players</h3>
+          </PlayerList>
+          <GameViewNew game={game} />
+        </>
+      )}
+      {game.gameStatus === "playing" && (
+        <>
+          {status.currentViewedGame?.currentPlayer.address ===
+          status.playerAddr ? (
             <>
               <PlayerList
-                players={status.currentViewedGame?.players as Player[]}
+                players={
+                  status.currentViewedGame.sessionName ===
+                  status.currentPlayingGame?.sessionName
+                    ? (status.currentPlayingGame?.players as Player[])
+                    : (status.currentViewedGame.players as Player[])
+                }
               >
                 <h3 className="font-bold">Players</h3>
               </PlayerList>
-              <GameViewNew game={game} />
+              <GameViewText />
             </>
+          ) : (
+            <p>It's {status.currentViewedGame?.currentPlayer.name}'s turn</p>
           )}
-          {game.gameStatus === "playing" && (
-            <>
-              {status.currentViewedGame?.currentPlayer.address ===
-              status.playerAddr ? (
-                <>
-                  <PlayerList
-                    players={
-                      status.currentViewedGame.sessionName ===
-                      status.currentPlayingGame?.sessionName
-                        ? (status.currentPlayingGame?.players as Player[])
-                        : (status.currentViewedGame.players as Player[])
-                    }
-                  >
-                    <h3 className="font-bold">Players</h3>
-                  </PlayerList>
-                  <GameViewText />
-                </>
-              ) : (
-                <p>
-                  It's {status.currentViewedGame?.currentPlayer.name}'s turn
-                </p>
-              )}
-            </>
-          )}
-          {game.gameStatus === "closed" && <GameViewClosed game={game} />}
         </>
       )}
-    </AppCtx.Consumer>
+      {game.gameStatus === "closed" && <GameViewClosed game={game} />}
+    </>
   );
 };
 
