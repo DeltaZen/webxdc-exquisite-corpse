e22d00a9be22c73261ec9462521253624400c6af
M4ss1ck <4ndyraul@gmail.com>
2022-05-16 21:46:28 -0400

    add: tutorial

--- src/components/GameTutorial.tsx
+++ src/components/GameTutorial.tsx
@@ -2,9 +2,21 @@ import React from "react";
 
 const GameTutorial = () => {
   return (
-    <div className="wrap">
-      <h2 className="font-bold text-2xl">How to play</h2>
-      <p>Blah blah blah</p>
+    <div className="text-justify wrap max-w-prose">
+      <h2 className="text-2xl font-bold">How to play</h2>
+      <p className="w-full p-2 fl">
+        Start a new story or join another one. The first player chooses the name
+        and the number of rounds.
+      </p>
+      <p className="w-full p-2 fl">
+        Once the game starts, each player (except the first one) will see a hint
+        of the last words written by the previous player and must use them to
+        continue the development of the story.
+      </p>
+      <p className="w-full p-2 fl">
+        In the <span className="text-red-900">library</span> you can find the
+        finished games and appreciate the real art.
+      </p>
     </div>
   );
 };
