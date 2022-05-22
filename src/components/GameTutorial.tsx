46a0703661a5c6c886462a4dcf052f073172c640
M4ss1ck <4ndyraul@gmail.com>
2022-05-22 01:15:24 -0400

    add: variables to tutorial

--- src/components/GameTutorial.tsx
+++ src/components/GameTutorial.tsx
@@ -6,12 +6,15 @@ const GameTutorial = () => {
       <h2 className="text-2xl font-bold fl">How to play</h2>
       <p className="w-full p-2 px-4 fl">
         Start a <span className="text-red-900">new story</span> or join another
-        one. The first player chooses the name and the number of rounds.
+        one. The first player chooses the name, the number of rounds, the
+        minimum text length (in words) of every player input and the amount of
+        words each player will see of what the previous one wrote (as a{" "}
+        <span className="text-red-900">hint or spoiler</span>).
       </p>
       <p className="w-full p-2 px-4 fl">
-        Once the game starts, each player (except the first one) will see a{" "}
-        <span className="text-red-900">hint</span> of the last words written by
-        the previous player and must use them to continue the development of the{" "}
+        Once the game starts, each player (except the first one) will see the
+        above mentionated <span className="text-red-900">hint</span> and must
+        use them to continue the development of the{" "}
         <span className="text-red-900">story</span>.
       </p>
       <p className="w-full p-2 px-4 fl">
