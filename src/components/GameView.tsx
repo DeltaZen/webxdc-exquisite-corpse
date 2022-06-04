c65e963f3dba30f26e95aebae8090ef690c94cd7
M4ss1ck <4ndyraul@gmail.com>
2022-06-03 20:08:31 -0400

    fix: game instead of players when admin

--- src/components/GameView.tsx
+++ src/components/GameView.tsx
@@ -31,7 +31,7 @@ const GameView: React.FC<{ game: Corpse }> = ({ game }) => {
           ) : (
             <PlayerListAdmin
               adminAddr={status.playerAddr}
-              players={status.currentViewedGame?.players as Player[]}
+              game={status.currentViewedGame}
             />
           )}
           <GameViewNew game={game} />
@@ -56,11 +56,11 @@ const GameView: React.FC<{ game: Corpse }> = ({ game }) => {
               ) : (
                 <PlayerListAdmin
                   adminAddr={status.playerAddr}
-                  players={
+                  game={
                     status.currentViewedGame.sessionName ===
                     status.currentPlayingGame?.sessionName
-                      ? (status.currentPlayingGame?.players as Player[])
-                      : (status.currentViewedGame.players as Player[])
+                      ? status.currentPlayingGame
+                      : status.currentViewedGame
                   }
                 />
               )}
@@ -79,17 +79,20 @@ const GameView: React.FC<{ game: Corpse }> = ({ game }) => {
 export default GameView;
 
 const PlayerListAdmin: React.FC<{
-  players: Player[];
+  game: Corpse;
   adminAddr: string;
-}> = ({ players, adminAddr }) => {
+}> = ({ game, adminAddr }) => {
   return (
     <>
       <ul className="flex flex-col items-start justify-center max-w-2xl px-4 py-2 m-2 btn-style">
         <h3 className="font-bold">Players</h3>
-        {players.map((player: Player) => {
+        {game.players.map((player: Player) => {
           return (
-            <li key={player.name} className="flex flex-row clamp-1">
-              {player.name}
+            <li
+              key={player.address}
+              className="flex flex-row items-center justify-start"
+            >
+              <span className="mr-auto clamp-1">{player.name}</span>
               {player.address !== adminAddr && <Delete />}
             </li>
           );
@@ -99,19 +102,25 @@ const PlayerListAdmin: React.FC<{
   );
 };
 
-const Delete = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    className="inline-flex w-6 h-6 ml-2"
-    fill="none"
-    viewBox="0 0 24 24"
-    stroke="currentColor"
-    strokeWidth={2}
-  >
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-    />
-  </svg>
-);
+const Delete = () => {
+  const { status, setStatus } = React.useContext(AppCtx);
+  const deletePlayer = () => alert("Not implemented yet");
+  return (
+    <button onClick={deletePlayer} className="ml-2">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="w-6 h-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        strokeWidth={2}
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+        />
+      </svg>
+    </button>
+  );
+};
