e729d150a5a25ddfbcf9cc9c17cd66242daef02d
M4ss1ck <4ndyraul@gmail.com>
2022-06-05 20:28:50 -0400

    fix: bug with date on closed games

--- src/components/GameViewClosed.tsx
+++ src/components/GameViewClosed.tsx
@@ -3,12 +3,16 @@ import React from "react";
 const GameViewClosed: React.FC<{ game: Corpse }> = ({ game }) => {
   const text = game.corpse.join(" ");
   const authors = game.players.map((p) => p.name).join(", ");
+  console.log(new Date(game.date ?? 0));
+  const fecha = game.date
+    ? new Date(game.date).toLocaleDateString("en-US")
+    : "-";
   return (
     <div className="max-w-prose flex flex-col items-center justify-center">
       <p className="my-2 font-serif text-red-900 ml-auto">By: {authors}</p>
-      {game.date && (
+      {fecha && (
         <p className="font-thin text-base text-red-900 my-2 font-mono ml-auto">
-          {game.date.toLocaleDateString("en")}
+          {fecha}
         </p>
       )}
       <p className="px-2 first-letter:font-fancy first-letter:text-6xl">
